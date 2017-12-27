f49926ed32608cd92d187e66704c9abfed1746ee
hacker1024 <joshleivenzon@outlook.com>
2017-12-27 14:26:17 +0000

    Fix up some key mapping, the action buttons should map like an xbox now.

--- linux/include/hid.h
+++ linux/include/hid.h
@@ -16,12 +16,12 @@ enum {
     HID_KEY_DLEFT        = BIT(5),  ///< D-Pad Left
     HID_KEY_DUP          = BIT(6),  ///< D-Pad Up
     HID_KEY_DDOWN        = BIT(7),  ///< D-Pad Down
-    HID_KEY_R            = BIT(8),  ///< R
-    HID_KEY_L            = BIT(9),  ///< L
-    HID_KEY_X            = BIT(10), ///< X
-    HID_KEY_Y            = BIT(11), ///< Y
-    HID_KEY_ZL           = BIT(14), ///< ZL (New 3DS only)
-    HID_KEY_ZR           = BIT(15), ///< ZR (New 3DS only)
+    HID_KEY_R            = BIT(15),  ///< R
+    HID_KEY_L            = BIT(14),  ///< L
+    HID_KEY_X            = BIT(11), ///< X
+    HID_KEY_Y            = BIT(10), ///< Y
+    HID_KEY_ZL           = BIT(9), ///< ZL (New 3DS only)
+    HID_KEY_ZR           = BIT(8), ///< ZR (New 3DS only)
     HID_KEY_TOUCH        = BIT(20), ///< Touch (Not actually provided by HID)
     HID_KEY_CSTICK_RIGHT = BIT(24), ///< C-Stick Right (New 3DS only)
     HID_KEY_CSTICK_LEFT  = BIT(25), ///< C-Stick Left (New 3DS only)
@@ -34,13 +34,13 @@ enum {
 
     // Generic catch-all directions
     /// D-Pad Up or Circle Pad Up
-    HID_KEY_UP = HID_KEY_DUP | HID_KEY_CPAD_UP,
+        HID_KEY_UP = HID_KEY_DUP | HID_KEY_CPAD_UP,
     /// D-Pad Down or Circle Pad Down
-    HID_KEY_DOWN = HID_KEY_DDOWN | HID_KEY_CPAD_DOWN,
+        HID_KEY_DOWN = HID_KEY_DDOWN | HID_KEY_CPAD_DOWN,
     /// D-Pad Left or Circle Pad Left
-    HID_KEY_LEFT = HID_KEY_DLEFT | HID_KEY_CPAD_LEFT,
+        HID_KEY_LEFT = HID_KEY_DLEFT | HID_KEY_CPAD_LEFT,
     /// D-Pad Right or Circle Pad Right
-    HID_KEY_RIGHT = HID_KEY_DRIGHT | HID_KEY_CPAD_RIGHT,
+        HID_KEY_RIGHT = HID_KEY_DRIGHT | HID_KEY_CPAD_RIGHT,
 };
 
 struct circlepos {
